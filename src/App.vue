63d1e9919c4fce7c6f52d9d75dd34663fc6b0adc
jojun0 <jojun01835@gmail.com>
2023-08-08 18:00:26 +0900

    add vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="floop in menus" :key="floop">{{ floop }}</a>
+  </div>
+
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>50만원</p>
+    <button @click="increase">허위매물신고</button> <span>신고수 : {{ report[0] }}</span>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>70만원</p>
+    <button @click="increase">허위매물신고</button> <span>신고수 : {{ report[1] }}</span>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>40만원</p>
+    <button @click="increase">허위매물신고</button> <span>신고수 : {{ report[2] }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      report: [0, 0, 0],
+      menus: ["Home", "products", " About"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
+  },
+  methods: {
+    increase() {
+      this.report += 1;
+    },
+  },
+  components: {},
+};
 </script>
 
 <style>
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
