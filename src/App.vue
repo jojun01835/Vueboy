718c3690e5dfe0f0f5cb154a936fac929ffc2c57
jojun0 <jojun01835@gmail.com>
2023-08-09 17:56:10 +0900

    add

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
+  <Modal />
   <div class="menu">
     <a v-for="floop in menus" :key="floop">{{ floop }}</a>
   </div>
-
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p>50만원</p>
-    <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{ report[0] }}</span>
-  </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>70만원</p>
-    <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{ report[1] }}</span>
-  </div>
-  <div>
-    <h4>{{ products[2] }}</h4>
-    <p>40만원</p>
-    <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{ report[2] }}</span>
+  <Discnt />
+  <div v-for="(a, i) in onerooms" :key="i">
+    <img :src="a.image" class="room-img" />
+    <h4
+      @click="
+        Modal = true;
+        click = i;
+      "
+    >
+      {{ a.title }}
+    </h4>
+    <p>{{ a.price }}</p>
   </div>
 </template>
 
 <script>
+import data from "./oneroom";
+import Discnt from "./Discnt.vue";
+import Modal from "./Modal.vue";
+
 export default {
   name: "App",
   data() {
     return {
+      click: 0,
+      onerooms: data,
+      Modal: false,
       report: [0, 0, 0],
       menus: ["Home", "products", " About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
@@ -35,11 +40,38 @@ export default {
       this.report[idx] += 1;
     },
   },
-  components: {},
+  components: {
+    Discnt,
+    Modal,
+  },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
 .menu {
   background: darkslateblue;
   padding: 15px;
---
+++ src/Modal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="black-bg" v-if="Modal == true">
+    <div class="white-bg">
+      <h4>{{ onerooms[click].title }}</h4>
+      <img :src="onerooms[click].image" :alt="onerooms[click].title" />
+      <h4>{{ onerooms[click].price }}</h4>
+      <p>{{ onerooms[click].content }}</p>
+      <button @click="Modal = false">❌</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "myModal",
+};
+</script>
+<style></style>
